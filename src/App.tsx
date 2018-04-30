bf1e6607903813eb35f51a2a56c1895b61c1084c
Ralph Effting <ralfting@gmail.com>
2018-04-30 12:20:24 -0300

    Update App.tsx
    Remove file name `index` to home.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Link, Route } from 'react-router-dom'
 
-import Home from "./pages/Home/index";
+import Home from "./pages/Home";
 
 import styles from "./App.block.css";
 
