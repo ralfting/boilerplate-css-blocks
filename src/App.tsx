1bd1f0bb08cba164ba6fb7c6ce521308a4d06216
Ralph Effting <ralfting@gmail.com>
2018-04-30 11:46:16 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+
+import Home from "./pages/Home/index";
+
+import styles from "./App.block.css";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className={styles}>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+            </ul>
+          </nav>
+          <main>
+            <Route exact path="/" component={Home} />
+          </main>
+          <footer className={styles.footer}>
+            Footer
+          </footer>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
